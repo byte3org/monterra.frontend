8ff0edf305234f2960c34640ad31f1b5ccf75701
rxOred <rxored@gmail.com>
2023-10-09 16:24:33 +0530

    initial commit

---
+++ lib/providers/navigation_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NavigationProvider {
+  // Implement your navigation logic here, e.g., push, pop, etc.
+  static void navigateToTasks(BuildContext context, String? token) {
+    //Navigator.of(context).push();
+  }
+}
